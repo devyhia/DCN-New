2ebe7de0a2a881dddfea3534ab812fbeab1e062a
Yehya Abouelnaga <devyhia@aucegypt.edu>
2018-11-10 15:44:34 +0100

    Control training stage using cli args

--- run_raw_coil20.py
+++ run_raw_coil20.py
@@ -23,13 +23,13 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Run DCN Training on COIL20 dataset')
     parser.add_argument('--dimension', type=int, default=28)
     parser.add_argument('--training_epochs', type=int, default=50)
-
+    parser.add_argument('--stage', choices=['layerwise', 'endtoend'])
+    parser.add_argument('--labels', type=int, default=20)
+    parser.add_argument('--trials', type=int, default=1)
     return parser.parse_args()
 
 args = parse_arguments()
 
-K = 20
-trials = 1
 
 features_path = "./data/coil20_features_{}.npy".format(args.dimension)
 labels_path = "./data/coil20_labels_{}.npy".format(args.dimension)
@@ -40,50 +40,51 @@ dataset = (features_path, labels_path)
 
 #  need to train with 250 epochs of layerwise, and 250 epochs of end-end SAE
 #  to get the initialization file with the following setting, takes a while
+if args.stage == 'layerwise':
+    config = {'Init': '',
+              'lbd':  1,  # reconstruction
+              'beta': 0,
+              'output_dir': 'COIL20_results',
+              'save_file': 'coil20_pre_{}.pkl.gz'.format(args.dimension),
+              'pretraining_epochs': 250,
+              'pretrain_lr_base': 0.0001,
+              'mu': 0.9,
+              'finetune_lr': 0.0001,
+              'training_epochs': 250,
+              'dataset': dataset,
+              'batch_size': 240,
+              'nClass': args.labels,
+              'hidden_dim': [500, 500, 2000, 10],
+              'diminishing': False}
 
-# config = {'Init': '',
-          # 'lbd':  1,  # reconstruction
-          # 'beta': 0,
-          # 'output_dir': 'COIL20_results',
-          # 'save_file': 'coil20_pre.pkl.gz',
-          # 'pretraining_epochs': 250,
-          # 'pretrain_lr_base': 0.0001,
-          # 'mu': 0.9,
-          # 'finetune_lr': 0.0001,
-          # 'training_epochs': 250,
-          # 'dataset': dataset,
-          # 'batch_size': 240,
-          # 'nClass': K,
-          # 'hidden_dim': [500, 500, 2000, 10],
-          # 'diminishing': False}
-
-config = {'Init': 'coil20_pre.pkl.gz',
-          'lbd':  1,  # reconstruction
-          'beta': 1,
-          'output_dir': 'COIL20_results',
-          'save_file': 'coil20_10.pkl.gz',
-          'pretraining_epochs': 250,
-          'pretrain_lr_base': 0.0001,
-          'mu': 0.9,
-          'finetune_lr': 0.0001,
-          'training_epochs': 250,
-          'dataset': dataset,
-          'batch_size': 240,
-          'nClass': K,
-          'hidden_dim': [500, 500, 2000, 10],
-          'diminishing': False}
+if args.stage == 'endtoend':
+    config = {'Init': 'coil20_pre.pkl.gz',
+              'lbd':  1,  # reconstruction
+              'beta': 1,
+              'output_dir': 'COIL20_results',
+              'save_file': 'coil20_10_{}.pkl.gz'.format(args.dimension),
+              'pretraining_epochs': 250,
+              'pretrain_lr_base': 0.0001,
+              'mu': 0.9,
+              'finetune_lr': 0.0001,
+              'training_epochs': 250,
+              'dataset': dataset,
+              'batch_size': 240,
+              'nClass': args.labels,
+              'hidden_dim': [500, 500, 2000, 10],
+              'diminishing': False}
 
 __import__('pdb').set_trace()
 results = []
-for i in range(trials):
+for i in range(args.trials):
     res_metrics = test_SdC(**config)
     results.append(res_metrics)
 
-results_SAEKM = np.zeros((trials, 3))
-results_DCN = np.zeros((trials, 3))
+results_SAEKM = np.zeros((args.trials, 3))
+results_DCN = np.zeros((args.trials, 3))
 
 N = config['training_epochs']/5
-for i in range(trials):
+for i in range(args.trials):
     results_SAEKM[i] = results[i][0]
     results_DCN[i] = results[i][N]
 SAEKM_mean = np.mean(results_SAEKM, axis=0)
